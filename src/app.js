60dae5354d1e7f0536b2b7cb269b28b557c36b3d
miscode <nmamadoundir@gmail.com>
2025-05-03 17:57:33 +0200

    version 3

--- src/app.js
+++ src/app.js
@@ -5,11 +5,23 @@ const container = document.getElementById('container');
 
 //fonctionnalité1 : ajouter une todo à sa to do list
 
+
+
 const added = () => {
-    El = document.createElement('div');
-    texte = document.createElement('div');
-    suppr = document.createElement('button');
+    if (toDo.value === '') {
+        const warn = document.getElementById('warning');
+        warn.style.display = 'block';
+        warn.classList.add('opacity-100');
+        warn.classList.add('mt-0');
+        setTimeout(() => {
+            warn.style.display = 'none';
+        }, 3000)
+    }else {
+        const El = document.createElement('div');
+    const texte = document.createElement('div');
+    const suppr = document.createElement('button');
     suppr.textContent = 'supprimer';
+    suppr.classList.add('delete');
     suppr.classList.add('bg-black');
     suppr.classList.add('w-1/4');
     suppr.classList.add('text-xl');
@@ -19,6 +31,7 @@ const added = () => {
     suppr.classList.add('hover:bg-red-500');
     suppr.classList.add('hover:text-gray-400');
     suppr.classList.add('font-bold');
+    El.classList.add('element');
     El.classList.add('w-full');
     El.classList.add('flex');
     El.classList.add('justify-center');
@@ -31,14 +44,35 @@ const added = () => {
     texte.classList.add('flex');
     texte.classList.add('justify-center');
     texte.classList.add('items-center');
+    texte.classList.add('transition-all');
+    texte.classList.add('duration-300');
+    texte.classList.add('hover:bg-amber-200');
     texte.textContent = toDo.value;
     container.appendChild(El);
     El.appendChild(texte);
     El.appendChild(suppr);
 
-    suppr.onclick = () => {
-        El.style.display = 'none';
-    }
+    const btnsDelete = document.querySelectorAll('.delete');
+    const elements = document.querySelectorAll('.element');
+
+    btnsDelete.forEach((del, index) => {
+        del.onclick = () => {
+            elements[index].style.display = 'none';
+        }
+    })
+    
+    
+
+
+
 }
+}
+
+
+
+
+
+
+
 
 addButton.onclick = added;
